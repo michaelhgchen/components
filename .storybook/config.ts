7553d83698c1d795ce2e488a5ea28175cdc43d79
Michael Chen <michael.chen@opendoor.com>
2019-09-21 03:59:31 -0700

    add a11y storybook plugin + styled-components

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,15 +1,6 @@
-// import { configure } from "@storybook/react";
-// // automatically import all files ending in *.stories.tsx
-// const req = require.context("../src/stories/", true, /\.stories\.tsx$/);
-
-// function loadStories() {
-//   req.keys().forEach(req);
-// }
-
-// configure(loadStories, module);
-
-import { configure } from '@storybook/react';
+import { addDecorator, configure } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.tsx$/), module);
-
+configure(require.context('../src', true, /\.stories\.tsx$/), module);
+addDecorator(withA11y)
